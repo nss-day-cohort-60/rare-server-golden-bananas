c7890e6972ce0555632d847b2d84118621d5fda9
Charles Seals <charlesseals11@gmail.com>
2023-02-28 14:45:28 -0600

    register is now working

--- rareapi/models/rare_user.py
+++ rareapi/models/rare_user.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class RareUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=150)
-    profile_image_url = models.CharField(max_length=200)
+    profile_image_url = models.CharField(max_length=200, null=True, default="No URL Provided")
 
     @property
     def full_name(self):
--- rareapi/views/auth.py
+++ rareapi/views/auth.py
@@ -48,7 +48,8 @@ def register_user(request):
         username=request.data['username'],
         password=request.data['password'],
         first_name=request.data['first_name'],
-        last_name=request.data['last_name']
+        last_name=request.data['last_name'],
+        email=request.data['email']
     )
 
     # Now save the extra info in the levelupapi_gamer table
